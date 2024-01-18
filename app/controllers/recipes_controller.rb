f4e853ff2f9a9718026f9bf904cad6c1f6c2367c
Ismail mayito <ismayito1@gmail.com>
2024-01-18 09:20:21 +0300

    Add functionality of view files in the app

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,52 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+  @recipes = Recipe.all
+  end
 
-  def new; end
+  def destroy 
+  @recipe = Recipe.find(params[:id])
+  @recipe.destroy
+  flash[:notice] = 'Recipe Item successfully deleted.'
+    redirect_to recipes_path
+  end
 
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
+
+     # Debugging statements
+    #  logger.debug "Recipe ID: #{@recipe.id}"
+    #  logger.debug "Recipe Foods Count: #{@recipe_foods.count}"
+    #  @recipe_foods.each do |recipe_food|
+    #    logger.debug "Food Name: #{recipe_food.food.name}, Quantity: #{recipe_food.food.quantity}, Price: #{recipe_food.food.price}"
+    #  end
+  end
+  
+  #Action for public recipes
+  def public_recipes
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
+
+  def general_shopping_list
+    # Retrieve the current first user
+    @user = User.first
+
+    # Find all recipes of the logged-in user
+    @user_recipes = @user.recipes.includes(recipe_foods: :food)
+
+    # Retrieve the general food list of the user
+    @general_food_list = @user.foods
+
+    # Calculate the missing food items
+    @missing_food_items = @general_food_list - @user_recipes.flat_map { |recipe| recipe.foods }
+
+    # Calculate total quantity and total price of missing food items
+    @total_quantity = @missing_food_items.sum(&:quantity)
+    @total_price = @missing_food_items.sum(&:price)
+
+  end
+  
+  
 
   def create; end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,15 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :food_recipes
+  has_many :recipe_foods,dependent: :destroy
+
+
+  scope :public_foods, -> { where(public: true) }
+
+  def total_quantity
+    recipe_foods.sum(:quantity)
+  end
+
+  def total_price
+    recipe_foods.sum(:price)
+  end
 end
