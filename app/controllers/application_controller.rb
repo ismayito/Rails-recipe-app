afbec406ec57dd6b5cdae10db6ed7ab411865394
Ismail mayito <ismayito1@gmail.com>
2024-01-19 08:58:44 +0300

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    private
-    # Permits user name on top of authenticated email allowed by default on sign up
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  private
+
+  # Permits user name on top of authenticated email allowed by default on sign up
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
